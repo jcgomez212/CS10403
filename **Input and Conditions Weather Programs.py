10db3dae3da33e08dc5edc7fbba18c4f0b8e3cb1
jcgomez212 <91210626+jcgomez212@users.noreply.github.com>
2021-10-12 00:11:29 -0400

    conditions.py

---
+++ **Input and Conditions Weather Programs.py
@@ -0,0 +1,41 @@
+Python 3.9.7 (v3.9.7:1016ef3790, Aug 30 2021, 16:39:15) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> temp = 0
+>>> 
+= RESTART: /Users/julianagomez/Desktop/IDLE Algorithms/INPUT & CONDITIONS PROGRAM.py
+Traceback (most recent call last):
+  File "/Users/julianagomez/Desktop/IDLE Algorithms/INPUT & CONDITIONS PROGRAM.py", line 1, in <module>
+    while temp != 999:
+NameError: name 'temp' is not defined
+>>> 
+= RESTART: /Users/julianagomez/Desktop/IDLE Algorithms/INPUT & CONDITIONS PROGRAM.py
+Traceback (most recent call last):
+  File "/Users/julianagomez/Desktop/IDLE Algorithms/INPUT & CONDITIONS PROGRAM.py", line 1, in <module>
+    while temp != 999:
+NameError: name 'temp' is not defined
+>>> 
+= RESTART: /Users/julianagomez/Desktop/IDLE Algorithms/INPUT & CONDITIONS PROGRAM.py
+Traceback (most recent call last):
+  File "/Users/julianagomez/Desktop/IDLE Algorithms/INPUT & CONDITIONS PROGRAM.py", line 3, in <module>
+    while temp != 999:
+NameError: name 'temp' is not defined
+>>> 
+= RESTART: /Users/julianagomez/Desktop/IDLE Algorithms/INPUT & CONDITIONS PROGRAM.py
+Please enter the current temperature or 999 to end: 98
+Wear Shorts
+Please enter the current temperature or 999 to end: 90
+Short sleeves are fine
+Please enter the current temperature or 999 to end: 87
+Short sleeves are fine
+Please enter the current temperature or 999 to end: 70
+Wear a jacket
+Please enter the current temperature or 999 to end: 65
+Wear a jacket
+Please enter the current temperature or 999 to end: 50
+Wear a heavy coat
+Please enter the current temperature or 999 to end: 48
+Wear a heavy coat
+Please enter the current temperature or 999 to end: 32
+Stay inside
+Please enter the current temperature or 999 to end: 
