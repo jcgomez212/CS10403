132b5a72f082d9a1cdd11c6314d3d2a7f7409b07
jcgomez212 <91210626+jcgomez212@users.noreply.github.com>
2021-12-05 18:50:11 -0500

    PythonQueue

---
+++ PythonQueue.py
@@ -0,0 +1,20 @@
+>>> names = ["Jim","Bill","Abby","Jeff","Sam","Chris","Emily","Rob","Ryan","Eric"]
+for x in range(0,10):
+    names.append(0)
+    names.append(1)
+    names.append(2)
+    names.append(3)
+    names.append(4)
+    names.append(5)
+    names.append(6)
+    names.append(7)
+    names.append(8)
+    names.append(9)
+    names.append(10)
+    
+>>> for x in range(0,10):
+    len(names)
+    print(names.pop(0))
+
+
+
